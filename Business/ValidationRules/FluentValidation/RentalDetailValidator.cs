3a268ac1d217aba355237aa12abb0de6dac22abf
Furkan BİÇER <1030416934@erciyes.edu.tr>
2021-02-24 01:15:10 +0300

    .

--- Business/ValidationRules/FluentValidation/RentalDetailValidator.cs
+++ Business/ValidationRules/FluentValidation/RentalDetailValidator.cs
@@ -14,7 +14,7 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(r => r.CustomerId).NotEmpty();
             RuleFor(r => r.Id).NotEmpty();
             RuleFor(r => r.RentDate).NotEmpty();
-            RuleFor(r => r.ReturnDate).NotEmpty();
+            
         }
     }
 }
